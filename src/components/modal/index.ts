cb68f65623cf9795ed795b715f6f2370c0be5474
steveasterafovo <steveasterafovo@gmail.com>
2026-07-12 14:28:17 +0100

    s

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -1,6 +1,3 @@
 export { Modal } from "./Modal";
 export { CreateShortlinkModal } from "./CreateShortlinkModal";
 export { ReportModal } from "./ReportModal";
-export { RegistrationModal, RegistrationModal as TrainningRegistration } from "./TrainningRegistration";
-export { ShareModal } from "./ShareModal";
-export { SearchDialog, SearchTrigger } from "./SearchDialog";
