376b28449dc4126f7a8a118254dbdaeb2ff9dd37
steveasterafovo <steveasterafovo@gmail.com>
2026-07-25 19:33:38 +0100

    s

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -17,7 +17,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       { name: "title", content: SHORTEN_TITLE },
       { name: "description", content: SHORTEN_DESC },
       { name: "author", content: SITE.name },
-      { name: "keywords", content: `raccourcisseur, lien court, redirection securisee, transparence, spc Shortener, deconnexion, ${SITE.name}, porto-novo` },
+      { name: "keywords", content: `raccourcisseur, lien court, redirection securisee, transparence, spc shortener, deconnexion, ${SITE.name}, porto-novo` },
 
       // Open Graph / Facebook / LinkedIn
       { property: "og:title", content: SHORTEN_TITLE },
