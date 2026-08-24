b7622973d362dbdb4c7e126f04ec87a615fe89b6
steveasterafovo <steveasterafovo@gmail.com>
2026-08-24 14:10:39 +0100

    s

--- src/data/site.ts
+++ src/data/site.ts
@@ -3,12 +3,40 @@ const alias = isProd ? "ebdu5x" : "688ddw";
 
 export const SITE = {
   name: "STAF PRINT CENTER",
-  shortName: "SPC Shortener",
   slogan: "L'empreinte de votre succès",
   activity: "Studio de design et d'impression",
-  url: import.meta.env.VITE_SHORTSITE_URL,
-  landing: import.meta.env.VITE_FRONTEND_URL,
+  manager: "Steve Aster Afovo",
+  city: "Porto-Novo, Bénin",
+  phone: "+229 01 66 52 36 39",
+  whatsapp: "+229 01 60 30 06 07",
+  whatsappLink: "https://wa.me/2290160300607",
+  email: "contact@stafprint.com",
+  maps: "https://maps.app.goo.gl/4mbhWctm6LnTgYsAA",
+  notice: "https://search.google.com/local/reviews?placeid=ChIJPb8nmaNbOxARzgX2S_y9o_M",
+  socials: {
+    facebook: "https://web.facebook.com/StafPrintCenter",
+    instagram: "https://www.instagram.com/stafprintcenter/",
+    linkedin: "https://linkedin.com/company/stafprintcenter",
+    x: "https://x.com/stafprintcenter",
+  },
+  opinion: {
+    nb: "120+",
+    label: "avis clients",
+    stars: "4.9",
+  },
+
+  shortName: "SPC Shortener",
   alias,
+};
+
+export const SITE_LINK = {
+  landingUrl: import.meta.env.VITE_LANDING_URL,
+  shortUrl: import.meta.env.VITE_SHORTSITE_URL,
+  instructorUrl: import.meta.env.VITE_INSTRUCTOR_URL,
+  studentUrl: import.meta.env.VITE_STUDENT_URL,
+  meetUrl: import.meta.env.VITE_MEET_URL,
+  arcadeUrl: import.meta.env.VITE_ARCADE_URL,
+  docsUrl: import.meta.env.VITE_DOCS_URL,
 };
 
 // Message rédigé
