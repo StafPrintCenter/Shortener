fa0d0694020b2e9e1b61d9c2cb76e97ebb22c9d1
steveasterafovo <steveasterafovo@gmail.com>
2026-09-05 14:58:36 +0100

    s

--- src/components/modal/index.ts
+++ src/components/modal/index.ts
@@ -1,3 +1,4 @@
 export { Modal } from "./Modal";
 export { CreateShortlinkModal } from "./CreateShortlinkModal";
 export { ReportModal } from "./ReportModal";
+export { AllowedDomainsTooltip } from "./AllowedDomainsTooltip";
