b197f986947098e439548ab846dc131eff5d96c0
steveasterafovo <steveasterafovo@gmail.com>
2026-09-07 12:02:10 +0100

    s

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Outlet, createRootRouteWithContext, HeadContent, Scripts } from "@tanstack/react-router";
 import { type ReactNode } from "react";
-import { Toaster } from "@/components/ui/sonner";
+import { Toaster } from "sonner";
 import appCss from "../styles.css?url";
 import { NotFoundComponent, ErrorComponent } from "@/components/errors";
 import { SITE } from "@/data/site";
