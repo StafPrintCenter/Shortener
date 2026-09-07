02a7e7e452c05698d35848c2feef23f9b162285c
steveasterafovo <steveasterafovo@gmail.com>
2026-09-07 12:02:20 +0100

    s

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,8 @@ import { Outlet, createRootRouteWithContext, HeadContent, Scripts } from "@tanst
 import { type ReactNode } from "react";
 import { Toaster } from "sonner";
 import appCss from "../styles.css?url";
+import { SITE, SITE_LINK } from "@/data/site";
+import logo from "@/assets/logos.json";
 import { NotFoundComponent, ErrorComponent } from "@/components/errors";
 import { SITE } from "@/data/site";
 
